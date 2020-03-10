11c16489d40acff2d0702a8ae724bd9e3eb0d309
Wioleta <50668358+wioleta1501@users.noreply.github.com>
2020-03-10 20:45:46 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for (int i=1; i<=100; i++)
+    {
+        if (i%15==0) printf("%d - FizzBuzz\n", i);
+        else if (i%5==0) printf("%d - Buzz\n", i);
+        else if (i%3==0) printf("%d - Fizz\n", i);
+        else printf("%d\n", i);
+    }
+    return 0;
+}
